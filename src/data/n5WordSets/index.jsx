889a312f79b2a5a7a9ad08fe4d89b6e0c3f446f1
yutocode <example@gmail.com>
2025-07-31 12:18:41 +0900

    Fix QuizCard filename case mismatch

--- src/data/n5WordSets/index.jsx
+++ src/data/n5WordSets/index.jsx
@@ -3,6 +3,7 @@ import { n5part1 } from "./n5part1.js";
 import { n5part2 } from "./n5part2.js";
 
 export const n5WordSets = {
-  part1: n5part1,
-  part2: n5part2,
+  Lesson1: n5part1.Lesson1,
+  Lesson2: n5part2.Lesson2,
 };
+
